11e7e76197fdcb9b1dd11a9fb32b46283aa4acf8
Yuriy Golota <ypgolota@pm.me>
2022-06-22 13:59:41 -0400

    validate guess so as not to lose a turn if repeat guess

--- mystery_word.py
+++ mystery_word.py
@@ -1,15 +1,16 @@
 import random
 
 def decide_to_play():
-    print('#' * 80)
-    print(f'Would you like to play Mystery Word, Y/N?')
-    decision_to_play = input()
+    print('\n' + ('#' * 80) + '\n')
+    # print(f'Would you like to play Mystery Word, Y/N?')
+    decision_to_play = input('Would you like to play Mystery Word, Y/N? ')
 
     if decision_to_play == 'y' or decision_to_play == 'Y':
         play_game()
     elif decision_to_play == 'n' or decision_to_play == 'N':
         print('Well, okay then...')
         print('#' * 80)
+        STOP
     else:
         decide_to_play()
 
@@ -35,16 +36,17 @@ def play_game():
         print(f'Guesses: {guesses_str}')
         print(f'Mystery Word: {display_str}\n')
     
+    print_end_game_msg()
+    
 def print_game_instructions(answer_str):
-    print('#' * 80)
-    print('Instructions:')
-    print('1. You will have 8 turns at guessing characters for the answer.')
-    print('2. You can try to guess the mystery word at the end of every turn.\n')
+    print('\n' + ('#' * 80))
+    print('\nInstructions:')
+    print('1. You will have 8 turns at guessing characters for the Mystery Word.')
+    print('2. You can try to guess the Mystery Word at the end of every turn.\n')
     print(f'Answer: {str(answer_str)}')
     print(f'Mystery Word length: {len(answer_str)} characters')
-    print('Mystery Word: ' + ('_ ' * len(answer_str)))
-    print('#' * 80)
-    print('\n')
+    print('Mystery Word: ' + ('_ ' * len(answer_str)) + '\n')
+    print(('#' * 80) + '\n')
 
 def generate_word_for_game():
     # Open words.txt as read
@@ -63,19 +65,21 @@ def generate_word_for_game():
     return upper_word_for_game_as_str
 
 def guess_character(guesses_list):
-    guess = input('Guess a letter: ')
-    if validate_guess(guess) == False:
+    guess = input('Guess a letter: ').upper()
+    if validate_guess(guess, guesses_list) == False:
         guess_character(guesses_list)
     else: 
-        guess = guess.upper()
         return guesses_list.append(guess)
 
-def validate_guess(guess):
+def validate_guess(guess, guesses_list):
     if len(guess) != 1:
         print('\nGuess a single letter at a time.')
         return False
     elif guess.isalpha() == False:
         print('\nYour guess must be a letter.')
+        return False
+    elif guess in guesses_list:
+        print(f'You\'ve already guessed {guess}, try again.' + '\n')
         return False
 
 if __name__ == "__main__":
