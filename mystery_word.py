d37d82f54860cb943c29cd1cdb7123270a4cdce8
Yuriy Golota <ypgolota@pm.me>
2022-06-22 16:51:29 -0400

    refactor: decide_to_play() for better appearance in the terminal

--- mystery_word.py
+++ mystery_word.py
@@ -2,15 +2,13 @@ import random
 
 def decide_to_play():
     print('\n' + ('#' * 80) + '\n')
-    # print(f'Would you like to play Mystery Word, Y/N?')
-    decision_to_play = input('Would you like to play Mystery Word, Y/N? ')
+    decision_to_play = input('Would you like to play Mystery Word, Y/N? ').upper()
 
-    if decision_to_play == 'y' or decision_to_play == 'Y':
+    if decision_to_play == 'Y':
         play_game()
-    elif decision_to_play == 'n' or decision_to_play == 'N':
-        print('Well, okay then...')
-        print('#' * 80)
-        STOP
+    elif decision_to_play == 'N':
+        print('\nWell, okay then...')
+        print('\n' + ('#' * 80) + '\n')
     else:
         decide_to_play()
 
@@ -23,7 +21,7 @@ def play_game():
     print_game_instructions(answer_str)
 
     while turns_remaining > 0:
-        guess = guess_character(guesses_list)
+        guess = guess_letter(guesses_list)
         guesses_list.append(guess)
         guesses_str = str(' '.join(guesses_list))
 
@@ -51,14 +49,14 @@ def play_game():
         # print(f'display_list: {display_list}\n')
     
 def print_game_instructions(answer_str):
-    print('\n' + ('#' * 80))
-    print('\nInstructions:')
+    print('\n' + ('#' * 80) + '\n')
+    print('Instructions:')
     print('1. You will have 8 turns at guessing characters for the Mystery Word.')
     print('2. You can try to guess the Mystery Word at the end of every turn.\n')
     print(f'Answer: {str(answer_str)}')
     print(f'Mystery Word length: {len(answer_str)} characters')
-    print('Mystery Word: ' + ('_ ' * len(answer_str)) + '\n')
-    print(('#' * 80) + '\n')
+    print('Mystery Word: ' + ('_ ' * len(answer_str)))
+    print('\n' + ('#' * 80) + '\n')
 
 def generate_word_for_game():
     # Open file as read
@@ -81,19 +79,19 @@ def generate_word_for_game():
     # Return word for the game
     return word_str_upper
 
-def guess_character(guesses_list):
+def guess_letter(guesses_list):
     guess = input('Guess a letter: ').upper()
 
     # Validating the guess
     if len(guess) != 1:
         print('\nGuess a single letter at a time.\n')
-        return guess_character(guesses_list)
+        return guess_letter(guesses_list)
     elif guess.isalpha() == False:
         print('\nYour guess must be a letter.\n')
-        return guess_character(guesses_list)
+        return guess_letter(guesses_list)
     elif guess in guesses_list:
         print(f'\nYou\'ve already guessed {guess}, try again.' + '\n')
-        return guess_character(guesses_list)
+        return guess_letter(guesses_list)
     else:
         return guess
 
