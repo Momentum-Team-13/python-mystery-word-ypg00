e13f5576cd88d80c6583ae3ed227873d396907e8
Yuriy Golota <ypgolota@pm.me>
2022-06-22 17:28:44 -0400

    refactor: improved terminal display

--- mystery_word.py
+++ mystery_word.py
@@ -1,62 +1,62 @@
 import random
 
+def play_game():
+    guesses_list = []
+    turns_remaining = 8
+
+    decide_to_play()
+    ANSWER_STR = generate_word_for_game()
+    ANSWER_LIST = list(ANSWER_STR)
+    print_game_instructions(ANSWER_STR, turns_remaining)
+
+    while turns_remaining > 0:
+        guess = guess_letter(guesses_list)
+        guesses_list.append(guess)
+        # Casting the list of guesses as a string for cleaner terminal presentation
+        guesses_str = str(' '.join(guesses_list))
+
+        # Check guesses_list against ANSWER_LIST for matches
+        display_list = [(char.replace(char, "_")) if char not in guesses_list else char for char in ANSWER_LIST]
+        # Casting the list as a string for cleaner terminal presentation
+        display_str = ' '.join(display_list)
+
+        # Check if solved
+        if display_list == ANSWER_LIST:
+            print('\nYou solved it!\n')
+            print(f'The Mystery Word was {ANSWER_STR}.\n')
+            break
+        
+        # Lose a turn if guess is incorrect
+        if guess not in ANSWER_LIST:
+            turns_remaining -= 1
+        
+        # Print status report
+        print('\n' + '#' * 80 + '\n')
+        print(f'Turns remaining: {turns_remaining}')
+        print(f'Guesses: {guesses_str}\n')
+        print(f'Mystery Word: {display_str}\n')
+    
 def decide_to_play():
     print('\n' + ('#' * 80) + '\n')
     decision_to_play = input('Would you like to play Mystery Word, Y/N? ').upper()
 
     if decision_to_play == 'Y':
-        play_game()
+        pass
     elif decision_to_play == 'N':
         print('\nWell, okay then...')
         print('\n' + ('#' * 80) + '\n')
     else:
         decide_to_play()
 
-def play_game():
-    answer_str = generate_word_for_game()
-    answer_list = list(answer_str)
-    guesses_list = []
-    turns_remaining = 8
-    
-    print_game_instructions(answer_str)
-
-    while turns_remaining > 0:
-        guess = guess_letter(guesses_list)
-        guesses_list.append(guess)
-        guesses_str = str(' '.join(guesses_list))
-
-        # Lose a turn if guess is incorrect
-        if guess not in answer_list:
-            turns_remaining -= 1
-
-        # Check guesses_list against answer_list to create what to display
-        display_list = [(char.replace(char, "_")) if char not in guesses_list else char for char in answer_list]
-        display_str = ' '.join(display_list)
-
-        # Check if solved
-        if display_list == answer_list:
-            print('\nYou solved it!')
-            print(f'The Mystery Word was {answer_str}.')
-            break
-        
-        # Print status report
-        print(f'\nTurns remaining: {turns_remaining}')
-        print(f'Guesses: {guesses_str}')
-        print(f'Mystery Word: {display_str}\n')
-
-        # print(f'guesses_list: {guesses_list}')
-        # print(f'answer_list: {answer_list}')
-        # print(f'display_list: {display_list}\n')
-    
-def print_game_instructions(answer_str):
+def print_game_instructions(ANSWER_STR, turns_remaining):
     print('\n' + ('#' * 80) + '\n')
-    print('Instructions:')
-    print('1. You will have 8 turns at guessing characters for the Mystery Word.')
-    print('2. You can try to guess the Mystery Word at the end of every turn.\n')
-    print(f'Answer: {str(answer_str)}')
-    print(f'Mystery Word length: {len(answer_str)} characters')
-    print('Mystery Word: ' + ('_ ' * len(answer_str)))
+    print('Instructions:\n')
+    print(f'1. You are allowed {turns_remaining} incorrect guesses before losing the game.')
+    print('2. You can try to guess the Mystery Word at the end of every turn.')
+    print(f'3. The Mystery Word is {len(ANSWER_STR)} characters long.')
     print('\n' + ('#' * 80) + '\n')
+    print('Mystery Word: ' + ('_ ' * len(ANSWER_STR)) + '\n')
+    # print(f'Answer: {str(ANSWER_STR)}')
 
 def generate_word_for_game():
     # Open file as read
@@ -99,4 +99,4 @@ def print_end_game_msg():
     pass
 
 if __name__ == "__main__":
-    decide_to_play()
+    play_game()
